930077b892b219180ed1e30e8b9c31066dd8d974
maciekole <24421260+maciekole@users.noreply.github.com>
2023-10-30 21:47:42 +0100

    middleware, security and requests logging

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,8 +2,7 @@ package main
 
 import "net/http"
 
-// The routes() method returns a servemux containing application routes.
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/*"))
@@ -13,5 +12,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return app.logRequest(secureHeaders(mux))
 }
